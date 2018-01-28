d74c87f6b46b460023f33d2103af3d2972e5826e
mohitesh07 <agarwalmohitesh@gmail.com>
2018-01-28 10:37:49 +0530

    helloworld.c program added.

---
+++ Programs/helloworld.c
@@ -0,0 +1,9 @@
+/* my first C program */
+#include <stdio.h>
+
+int main()
+{
+    //printf() function returns the string within quotes
+    printf("Hello, world!\n");
+    return 0;
+}//end of main
